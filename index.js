55cbd29523e3f2b39ceba200cc5db494271de510
xotamov <xotamovazizjon997@gmail.com>
2023-05-30 18:06:10 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+let name = prompt('What is your name')
+let koname = "Alex"
+let money = 10000
+if(koname === name){
+    let hopmoney =prompt('Your hopmoney Should be 7777')
+    if(hopmoney === '7777'){
+        let cash = prompt('How much your cash')
+        if(cash <= money){
+            alert(
+                `Successful
+                your balance was: ${money}
+                your balance now: ${money - cash}
+                `
+            )
+        } else{
+            console.log('Недосточно средство');
+        } 
+    } else{
+        console.log('Ползовател не найден');
+    }
+} else{
+    console.log('go home');
+}
+let oldname = prompt('What is your name')
+charAt
+if(oldname.charAt() === 'Alex'){
+    let age = prompt('How old are you')
+    if(age >= 20 && old <= 40){
+        let many = prompt('How much your many')
+        if(many >= '100'){
+            let people = prompt('how many people')
+            if(people <= 10){
+
+            } else{
+                ('you are a lot of people')
+            }
+        } else{
+            alert('you are poor boy')
+        }
+    } else{
+        alert('you are too old or too young')
+    }
+} else('go home')
